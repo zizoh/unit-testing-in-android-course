24bfa0f06a3371560a400c23c1211ea3e715d8d5
zizoh <zizoh@softcom.ng>
2020-07-25 18:54:29 +0100

    Exercise1 done

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,36 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class NegativeNumberValidatorTest {
+
+    private NegativeNumberValidator SUT;
+
+    @Test
+    public void isNegative_withZero_returnsFalse() {
+        SUT = new NegativeNumberValidator();
+        boolean result = SUT.isNegative(0);
+
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void isNegative_withPositiveNumber_returnsFalse() {
+        SUT = new NegativeNumberValidator();
+        boolean result = SUT.isNegative(1);
+
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void isNegative_withNegativeNumber_returnsTrue() {
+        SUT = new NegativeNumberValidator();
+        boolean result = SUT.isNegative(-1);
+
+        Assert.assertThat(result, is(true));
+    }
 
 }
